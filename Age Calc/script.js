cb9582b1d72a47ae54b5212a82821bac1c2dcebf
Oz Parl <ozzy7o@icloud.com>
2019-04-18 20:08:36 -0400

    Add files via upload

---
+++ Age Calc/script.js
@@ -0,0 +1,7 @@
+var userAge = prompt("What is your age?");
+
+var userAgeInDays = (userAge * 365);
+
+alert("You have been alive for " + userAgeInDays + " days." );
+
+console.log("You have been alive for " + userAgeInDays + " days." );
